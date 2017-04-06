db6d1b5e33dac1af62067a9e712662853c089638
faterror <stereodic@gmail.com>
2017-04-06 13:46:09 +1200

    Added actual code

---
+++ ComPortTest/ComPortTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ComPortTest
+{
+    class Program
+    {
+        public static bool running = true;
+        public static string instructions = @"Enter commands prefixed by a switch eg. /s hello
+/l - gets a list of available COM ports
+/c <x> - connects to COM port x
+/x - closes the currently opened COM port
+/s <message> - sends a message over the open COM port
+/r <message> re-sends a message at 5 second intervals
+/h - list of commands
+/q - exits the program";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(instructions);
+            while (running)
+            {
+                var command = Console.ReadLine();
+                ProcessCommand(command);
+            }
+        }
+
+        private static void ProcessCommand(string command)
+        {
+            if (!command.StartsWith("/") && command.Length < 2)
+            {
+                Console.WriteLine("Invalid Command");
+            }
+            else
+            {
+                var commandSwitch = command.Substring(0, 2);
+                command = command.Remove(0, 2);
+
+                switch (commandSwitch)
+                {
+                    case "/l":
+                        Serial.GetPorts();
+                        break;
+
+                    case "/c":
+                        Serial.OpenSerialPort(command);
+                        break;
+
+                    case "/x":
+                        Serial.CloseSerialPort();
+                        break;
+
+                    case "/s":
+                        Serial.WriteToSerialPort(command);
+                        break;
+
+                    case "/r":
+                        Serial.RepeatWrite(command);
+                        break;
+
+                    case "/h":
+                        Console.WriteLine(instructions);
+                        break;
+
+                    case "/q":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Unrecognized Command");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ComPortTest/ComPortTest/Serial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.IO.Ports;
+
+namespace ComPortTest
+{
+    class Serial
+    {
+        private static SerialPort _serialPort = new SerialPort("COM999", 9600, Parity.None, 8, StopBits.One);
+
+        public static void GetPorts()
+        {
+            Console.WriteLine("Getting list of COM ports...");
+            var ports = SerialPort.GetPortNames();
+            foreach (var port in ports)
+            {
+                Console.WriteLine(port);
+            }
+        }
+        public static void OpenSerialPort(string selectedPort)
+        {
+            _serialPort.PortName = "COM" + selectedPort;
+            try
+            {
+                Console.WriteLine("Trying to open Serial Port: {0}", _serialPort.PortName);
+                _serialPort.Open();
+                Console.WriteLine("Port {0} open, enter command", _serialPort.PortName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to open Serial Port {0}: {1}", _serialPort.PortName, ex);
+            }
+        }
+        public static void CloseSerialPort()
+        {
+            try
+            {
+                _serialPort.Close();
+                Console.WriteLine("Port {0} closed", _serialPort.PortName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to close Serial Port {0}: {1}", _serialPort.PortName, ex);
+            }
+        }
+        public static void WriteToSerialPort(string command)
+        {
+            if (_serialPort.IsOpen)
+            {
+                try
+                {
+                    byte[] instruction = Encoding.ASCII.GetBytes(command);
+                    _serialPort.Write(instruction, 0, instruction.Length);
+                    Console.WriteLine("Sending message: {0}", DateTime.Now);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to write to Port {0}: {1}", _serialPort.PortName, ex);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No open COM port");
+            }
+        }
+        public static void RepeatWrite(string command)
+        {
+            for (int i = 1; i >= 10; i++)
+            {
+                var timer = new System.Threading.Timer(
+                        e => WriteToSerialPort(command),
+                        null,
+                        TimeSpan.Zero,
+                        TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
